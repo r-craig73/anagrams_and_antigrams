391d188177803747519e9671175e57d42799c65e
Ron Craig <ron.craig@comcast.net>
2018-09-27 17:20:50 -0700

    add header line, remove whitespace, edit elsif condition

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,5 +1,5 @@
+# Anagrams class and compare_words method
 class Anagrams
-
   def initialize(word1)
     @word1 = word1
   end
@@ -7,20 +7,18 @@ class Anagrams
   def compare_words(input1, input2)
     input1 = @word1
     input1 = input1.gsub(/[ ';>.,!<?]/, '')
-    # matching_letters = input1.chars.sort() & input2.chars.sort()
-    if input1.chars.sort() == input2.chars.sort()
-      "These words are anagrams."
-    elsif input1.downcase().chars.sort() == input2.downcase().chars.sort()
-      "These words are anagrams."
-    elsif input1.scan(/[aeiouy]/).count == 0
-      "You need to input actual words!"
-    elsif input1.chars.sort() != input2.chars.sort()
-      "These words have no letter matches and are antigrams."
-    # elsif input1.chars.sort() != input2.chars.sort()
-    #   "#{matching_letters}"
+    if input1.chars.sort == input2.chars.sort
+      'The two words are anagrams.'
+    elsif input1.downcase.chars.sort == input2.downcase.chars.sort
+      'The two words are anagrams.'
+    elsif input1.scan(/[aeiouy]/).count.zero?
+      'Please input and actual word!'
+    elsif input1.chars.sort != input2.chars.sort
+      'The two words are antigrams.'
+    # elsif input1.chars.sort != input2.chars.sort
+    #   '#{matching_letters}'
     else
-      "What's going on???"
+      'Something is wrong :-( '
     end
   end
-
 end
